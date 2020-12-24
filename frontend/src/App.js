8a3a09dc45863aff45f04b366b46baa1ee8aed4e
Filip Balucha <balucha.filip2@gmail.com>
2020-12-24 01:33:01 +0100

    Replace photo stream with basic html file upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,12 @@
 import './App.css';
 import avocado from './avocado.png';
-import {useState, Fragment} from 'react';
-import { Camera } from "./camera";
-import { Root, Preview, Footer, GlobalStyle } from "./styles";
 
 function App() {
-  const [isCameraOpen, setIsCameraOpen] = useState(false);
-  const [cardImage, setCardImage] = useState();
-
   return (
     <div className="App">
       <img src={avocado} alt="" width="100px"/>
       <h1>Know your avocado!</h1>
-      <Fragment>
-      <Root>
-        {isCameraOpen && (
-          <Camera
-            onCapture={blob => setCardImage(blob)}
-            onClear={() => setCardImage(undefined)}
-          />
-        )}
-
-        {cardImage && (
-          <div>
-            <h2>Preview</h2>
-            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
-          </div>
-        )}
-
-        <Footer>
-          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
-          <button
-            onClick={() => {
-              setIsCameraOpen(false);
-              setCardImage(undefined);
-            }}
-          >
-            Close Camera
-          </button>
-        </Footer>
-      </Root>
-      <GlobalStyle />
-    </Fragment>
+      <input type="file" accept="image/*" capture="environment"></input>
     </div>
   );
 }
