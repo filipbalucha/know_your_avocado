3d019fdacb6d02742c6549f7bc0237a0d7b5bb91
Filip Balucha <balucha.filip2@gmail.com>
2021-01-06 16:52:15 +0100

    Code cleanup

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -2,26 +2,15 @@ import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Grid, Loader, Header, Popup } from "semantic-ui-react";
 
-const NUM_DP = 2;
-export const Result = (props) => {
-  const { loading, response } = props;
-  if (loading) {
-    return <Loader active>Loading</Loader>;
-  }
-  const { fruit_visible, result, summary } = response;
-  if (!fruit_visible) {
-    return (
-      <React.Fragment>
-        <Header as="h3">Sorry, I was unable to spot any 🥑</Header>
-        <p>Could you try retaking the photo?</p>
-      </React.Fragment>
-    );
-  }
-  // Create graph
+const Graph = (props) => {
+  const NUM_DP = 2;
+  const { result, summary } = props.response;
+
   const to_percentage = (val) => (val * 100).toFixed(NUM_DP);
   const probabilities = summary.probabilities.map(to_percentage);
   const probability = to_percentage(result.probability);
-  const ripeness = result.category.replace("avocado_", "");
+
+  const ripeness = result.category.replace("avocado_", ""); // TODO: temp
   const data = {
     labels: summary.categories,
     datasets: [
@@ -53,4 +42,23 @@ export const Result = (props) => {
       </Grid.Row>
     </Grid>
   );
+};
+
+const NotVisibleMessage = () => (
+  <React.Fragment>
+    <Header as="h3">Sorry, I was unable to spot any 🥑</Header>
+    <p>Could you try retaking the photo?</p>
+  </React.Fragment>
+);
+
+export const Result = (props) => {
+  const { loading } = props;
+  const { fruit_visible } = props.response;
+  if (loading) {
+    return <Loader active>Loading</Loader>;
+  } else if (fruit_visible) {
+    return <Graph {...props} />;
+  } else {
+    return <NotVisibleMessage />;
+  }
 };
