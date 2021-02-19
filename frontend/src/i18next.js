61cb35155ef14e766fce394e441ef68f03b5d85a
Filip Balucha <balucha.filip2@gmail.com>
2021-02-19 02:59:52 +0100

    Code cleanup

--- frontend/src/i18next.js
+++ frontend/src/i18next.js
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-// TODO: use Backend like in: https://react.i18next.com/latest/i18next-instance ?
 i18n
   .use(LanguageDetector)
   .use(initReactI18next) // bind react-i18next to the instance
