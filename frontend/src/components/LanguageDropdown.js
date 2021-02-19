559e464818419740e59be14b5d2272eaaadcf254
Filip Balucha <balucha.filip2@gmail.com>
2021-02-19 01:49:48 +0100

    Code cleanup

--- frontend/src/components/LanguageDropdown.js
+++ frontend/src/components/LanguageDropdown.js
@@ -1,22 +1,19 @@
-import { useContext } from "react";
 import { Icon, Form } from "semantic-ui-react";
-import ThemeContext from "../context/ThemeContext";
 
 export const LanguageDropdown = (props) => {
-  const { darkMode } = useContext(ThemeContext);
   const languageOptions = [
     { key: "English", text: "EN 🇬🇧", value: "gb" },
     { key: "Slovak", text: "SK 🇸🇰", value: "sk" },
   ];
   return (
-    <Form.Dropdown
-      inverted
-      item
-      button
-      text={<Icon name="world" />}
-      options={languageOptions}
-      onChange={(_, { value }) => props.setLanguage(value)}
-      // style={darkMode ? { backgroundColor: "red !important" } : null}
-    />
+    <Form>
+      <Form.Dropdown
+        item
+        button
+        text={<Icon name="world" />}
+        options={languageOptions}
+        onChange={(_, { value }) => props.setLanguage(value)}
+      />
+    </Form>
   );
 };
